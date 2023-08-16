3d7b2235f4300c46662cb9983a8469b9cf84a52f
upadhyaybs <upadhyaybs@live.com>
2023-08-16 18:35:48 -0500

    Updated for API Swagger

--- src/main/java/com/avarna/springboot/postgresql/example/resource/CustomerResource.java
+++ src/main/java/com/avarna/springboot/postgresql/example/resource/CustomerResource.java
@@ -2,6 +2,8 @@ package com.avarna.springboot.postgresql.example.resource;
 
 import com.avarna.springboot.postgresql.example.domain.Customer;
 import com.avarna.springboot.postgresql.example.service.ICustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,9 +14,11 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/customers")
 @RequiredArgsConstructor
+@Api(value = "Customer Management")
 public class CustomerResource {
 
     private final ICustomerService service;
+
 
     @PostMapping
     public Customer create(@RequestBody Customer customer){
@@ -22,21 +26,25 @@ public class CustomerResource {
     }
 
     @PutMapping
+    @ApiOperation(value = "Update a customer")
     public void update(@RequestBody Customer customer){
          service.update(customer.getId(),customer);
     }
 
     @GetMapping
+    @ApiOperation(value = "View a list of all the customers", response = List.class)
     public List<Customer> getAll(){
         return service.getAll();
     }
 
     @GetMapping("/id/{id}")
+    @ApiOperation(value = "Get a customer by its ID")
     public Customer findById(@PathVariable UUID id){
         return service.findById(id);
     }
 
     @DeleteMapping("/id/{id}")
+    @ApiOperation(value = "Delete a customer by its ID")
     public void delete(@PathVariable UUID id){
         service.delete(id);
     }
